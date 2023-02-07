e0fab485eef3c25ec911469b23da6910222beace
Kobi Shapira <shapirakobi@gmail.com>
2023-02-07 09:46:08 +0200

    parse known sites from Eli's DB

---
+++ Code/parse_known_sites.py
@@ -0,0 +1,26 @@
+import argparse
+import pandas as pd
+
+from General.argparse_utils import abs_path_from_str
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--editing_sites_excel_file", type=abs_path_from_str, help="Known sites file downloaded from https://www.tau.ac.il/~elieis/squid/")
+parser.add_argument("--sheet_name", help="For example, `D.pea Editing sites` or `O.vul Editing sites`")
+parser.add_argument("--out_csv_file", type=abs_path_from_str, help="Save specifc sheet_name in a separate csv file")
+parser.add_argument("--out_bed_file", type=abs_path_from_str, help="Parse editing sites to a bed file in this path")
+args = parser.parse_args()
+
+df = pd.read_excel(args.editing_sites_excel_file, sheet_name=args.sheet_name)
+df.to_csv(args.out_csv_file, index=False)
+bed_cols = ["Trinity id", "Editing location (base1)", "SwissProt name"]
+df2 = df.filter(bed_cols).rename(
+    columns={
+        "Trinity id": "#TrinityID",
+        "Editing location (base1)": "End",
+        "SwissProt name": "SwissProtName",
+    }
+)
+df2.insert(1, "Start", df2["End"] - 1)
+df2["Score"] = "."
+df2["Strand"] = "+"
+df2.to_csv(args.out_bed_file, index=False, sep="\t")
